d911d29078bb6b1bed753bdb11701eae76bffcc8
devsujalpatel <sujal7455@gmail.com>
2025-09-26 22:17:07 +0530

    prettier configured

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -39,20 +39,10 @@ export function Header() {
           {/* Rest of the header content */}
           <div className="flex items-center justify-center w-full flex-col">
             <div
-              className={`
-                            flex items-center justify-between
-                            bg-linear-to-b from-white/90 via-gray-50/90 to-white/90
-                            dark:from-zinc-900/90 dark:via-zinc-800/90 dark:to-zinc-900/90
-                            shadow-[0_2px_20px_-2px_rgba(0,0,0,0.1)]
-                            backdrop-blur-md
-                            border-x border-b 
-                            border-[rgba(230,230,230,0.7)] dark:border-[rgba(70,70,70,0.7)]
-                            w-full sm:min-w-[800px] sm:max-w-[1200px]
-                            rounded-b-[28px]
-                            px-4 py-2.5
-                            relative
-                            transition-all duration-300 ease-in-out
-                        `}
+              className=" flex items-center justify-between bg-linear-to-b from-white/90 via-gray-50/90 to-white/90
+                            dark:from-zinc-900/90 dark:via-zinc-800/90 dark:to-zinc-900/90  shadow-[0_2px_20px_-2px_rgba(0,0,0,0.1)]  backdrop-blur-md border-x border-b   border-[rgba(230,230,230,0.7)] dark:border-[rgba(70,70,70,0.7)]
+                            w-full sm:min-w-[800px] sm:max-w-[1200px] rounded-b-[28px] px-4 py-2.5 relative
+                            transition-all duration-300 ease-in-out"
             >
               <div className="relative z-10 flex items-center justify-between w-full gap-2">
                 {/* Logo Section with Navigation Links */}
